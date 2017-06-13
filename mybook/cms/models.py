bb040e7e84fb71d4ee16e919353ee3f538c7b2ee
kyosuke <kyosuke.araki611@gmail.com>
2017-06-13 11:28:00 +0900

    2 blank line

--- mybook/cms/models.py
+++ mybook/cms/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
     def __str__(self):
         return self.name
 
+
 class Impression(models.Model):
     """感想"""
     book = models.ForeignKey(Book, verbose_name='書籍', related_name='impressions')
