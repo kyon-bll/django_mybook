5db01db5b48f0067c6b72cde6dc570134a071200
kyosuke <kyosuke.araki611@gmail.com>
2017-06-16 17:52:40 +0900

    modify admin-page

--- mybook/cms/admin.py
+++ mybook/cms/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from cms.models import Book, Impression
 
-admin.site.register(Book)
-admin.site.register(Impression)
+
+# 最低限表示
+# admin.site.register(Book)
+# admin.site.register(Impression)
+
+# たくさん表示、リンクも生成
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publisher', 'page')  # 一覧に出す項目
+    list_display_links = ('id', 'name')  # リンクでとべるやつ
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class ImpressionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment')
+    list_display_links = ('id', 'comment')
+
+
+admin.site.register(Impression, ImpressionAdmin)
