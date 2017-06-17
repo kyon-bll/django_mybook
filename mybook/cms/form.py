9b15fe0b6c2fe9385e529fab61512332e4c8dc80
kyosuke <kyosuke.araki611@gmail.com>
2017-06-17 11:27:56 +0900

    form.py -> forms.py

--- mybook/cms/form.py
+++
@@ -1,9 +0,0 @@
-from django.form import ModelForm
-from cms.models import Book
-
-
-class BookForm(ModelForm):
-    """書籍のフォーム"""
-    class Meta:
-        model = Book
-        fields = ('name', 'publisher', 'page')
