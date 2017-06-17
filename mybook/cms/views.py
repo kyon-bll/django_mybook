c249f1b03d72b8c815fcd8ad54dbb54a2e54ee20
kyosuke <kyosuke.araki611@gmail.com>
2017-06-17 15:13:22 +0900

    Book;edit, delete

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from cms.models import Book
+from cms.forms import BookForm
 
 
 def book_list(request):
@@ -14,9 +15,28 @@ def book_list(request):
 
 
 def book_edit(request, book_id=None):
-    return HttpResponse('書籍の編集')
+    # return HttpResponse('書籍の編集')
+    if book_id:                 # 書籍の修正
+        book = get_object_or_404(Book, pk=book_id)
+    else:                       # 書籍の追加
+        book = Book()
+
+        # POSTでもGETでもformを作る
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)  # POSTされたデータからform作成
+        if form.is_valid():            # エラーがなければ保存 -> 一覧へ
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:                       # GETの時
+        form = BookForm(instance=book)
+
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
 
 def book_del(request, book_id):
     """書籍の削除"""
-    return HttpResponse('書籍の削除')
+    # return HttpResponse('書籍の削除')
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
